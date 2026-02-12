114613682b30e24319e2fa93be95e2788d9c3e6e
Cynthia Chang <aacindy1026@gmail.com>
2026-02-13 06:58:16 +0800

    能順利暫停/開始/設計時間

--- vt-website-main/work_overtime/get-countdown.php
+++ vt-website-main/work_overtime/get-countdown.php
@@ -1,7 +1,9 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Cache-Control: no-cache, must-revalidate');
+header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Pragma: no-cache');
+header('Expires: 0');
 
 $filepath = __DIR__ . '/countdown-data.json';
 
